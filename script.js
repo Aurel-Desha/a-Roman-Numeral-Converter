880336cb076cae610d7790c98f8445275ebea2f9
aurel-shael <deshyesguy2@gmail.com>
2024-03-18 03:09:23 +0100

    script

--- script.js
+++ script.js
@@ -4,75 +4,81 @@ const output = document.getElementById("output");
 
 const numralObject = [
   {
-    romanNumeral: 1,
-    arabicNumeral: 'I'
-  },
-   {
-    romanNumeral: 4,
-    arabicNumeral: 'IV'
-  },
-   {
-    romanNumeral: 5,
-    arabicNumeral: 'V'
-  },
-   {
-    romanNumeral: 9,
-    arabicNumeral: 'IX'
-  },
-   {
-    romanNumeral: 10,
-    arabicNumeral: 'X'
-  },
-   {
-    romanNumeral: 40,
-    arabicNumeral: 'XL'
-  },
-   {
-    romanNumeral: 50,
-    arabicNumeral: 'L'
-  },
-   {
-    romanNumeral: 90,
-    arabicNumeral: 'XC'
-  },
-   {
-    romanNumeral: 100,
-    arabicNumeral: 'C'
-  },
-   {
-    romanNumeral: 400,
-    arabicNumeral: 'CD'
-  },
-   {
-    romanNumeral: 500,
-    arabicNumeral: 'D'
+    romanNumeral: 1000,
+    arabicNumeral: 'M'
   },
   {
     romanNumeral: 900,
     arabicNumeral: 'CM'
   },
   {
-    romanNumeral: 1000,
-    arabicNumeral: 'M'
+    romanNumeral: 500,
+    arabicNumeral: 'D'
+  },
+
+  {
+    romanNumeral: 400,
+    arabicNumeral: 'CD'
+  },
+  {
+    romanNumeral: 100,
+    arabicNumeral: 'C'
+  },
+  {
+    romanNumeral: 90,
+    arabicNumeral: 'XC'
+  },
+  {
+    romanNumeral: 50,
+    arabicNumeral: 'L'
+  },
+  {
+    romanNumeral: 40,
+    arabicNumeral: 'XL'
+  },
+  {
+    romanNumeral: 10,
+    arabicNumeral: 'X'
+  },
+   {
+    romanNumeral: 9,
+    arabicNumeral: 'IX'
+  },
+   {
+    romanNumeral: 5,
+    arabicNumeral: 'V'
+  },
+    {
+    romanNumeral: 4,
+    arabicNumeral: 'IV'
+  },
+   {
+    romanNumeral: 1,
+    arabicNumeral: 'I'
   },
 ]
 
 convertBtn.addEventListener('click', ()=>{
-  if(number.value === ""){
+  const inputValue = parseInt(number.value, 10)
+  if(isNaN(inputValue)){
     output.textContent= "Please enter a valid number.";
-  }else if(number.value < 0){
+  }else if(inputValue < 0){
     output.textContent= "Please enter a number greater than or equal to 1";
-  }else if(number.value >= 4000){
+  }else if(inputValue >= 4000){
     output.textContent= "Please enter a number less than or equal to 3999";
   } else {
-    convert();
+    const romanNumeral = convert(inputValue);
+    output.textContent = `${romanNumeral}`;
   }
 })
 
-const convert = ()=> {
+const convert = (num)=> {
+  let roman = "";
   numralObject.forEach((obj)=>{
-    if(number.value === obj.romanNumeral) {
-      output.textContent = obj.arabicNumeral;
-    }
+    while(num >= obj.romanNumeral) {
+      roman +=  obj.arabicNumeral;    
+      num -= obj.romanNumeral;
+    } 
   })
+  return roman;
 }
